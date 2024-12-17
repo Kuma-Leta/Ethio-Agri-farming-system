4593bf494bd6f5c561f5f358fcd5f53edc9d0cf9
kuma <kumaleta2021@gmail.com>
2024-12-17 13:07:18 +0300

    define routes

--- back-end/src/routes/userRoutes.ts
+++ back-end/src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { registerUser } from "../controllers/userControllers/signup";
-
+import { loginUser } from "../controllers/userControllers/login";
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
